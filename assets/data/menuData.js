659b6ead6bd5bf50daaf3b1c5db8a890249ade05
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2023-12-06 18:15:56 +0530

    initial files added

---
+++ assets/data/menuData.js
@@ -0,0 +1,14 @@
+const mainDataArr = [
+  {
+    id: 1,
+    img: "dropdown_menu_img.png",
+    title: "Custom Dropdown Menu",
+    link: "dropdown_menu.html",
+    description:
+      "Added animation dropdown menu so it will have an effect of expanding smoothly.",
+    dayCreated: "wednesday",
+    dateCreated: "December 06, 2023",
+  }, 
+];
+
+export default mainDataArr;
