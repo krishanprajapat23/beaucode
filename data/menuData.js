d2c6266284232d63aa9b03700eb0c6001b61c35d
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2023-12-11 17:57:51 +0530

    code updated and back to top page added

---
+++ data/menuData.js
@@ -0,0 +1,24 @@
+const mainDataArr = [
+  {
+    id: 1,
+    img: "dropdown_menu_img.png",
+    title: "Custom Dropdown Menu",
+    link: "dropdown_menu.html",
+    description:
+      "Added animation dropdown menu so it will have an effect of expanding smoothly.",
+    dayCreated: "wednesday",
+    dateCreated: "December 06, 2023",
+  }, 
+  {
+    id: 2,
+    img: "backtotop.png",
+    title: "Back To Top Button",
+    link: "backToTop.html",
+    description:
+      "This 'Back to Top' link allows users to smoothly scroll back to the top of the page.",
+    dayCreated: "monday",
+    dateCreated: "December 11, 2023",
+  }, 
+];
+
+export default mainDataArr;
